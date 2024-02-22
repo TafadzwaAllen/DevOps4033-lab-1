db9dd87e5be36c97e5c657b51f2d0846cf32c059
Tafadzwa Mataire <matairetafadzwa@gmail.com>
2024-02-21 23:01:04 -0500

    lab-1

--- simpleCalc.js
+++ simpleCalc.js
@@ -4,9 +4,11 @@ var mathOperations = {
   sum: function (a, b) {
     return a + b
   },
+
   diff: function (a, b) {
     return a - b
   },
+  
   product: function (a, b) {
     return a * b
   },
